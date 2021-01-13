91d3282575b3887ae9ec6295bc25419c280d71df
Muhammad Rizki <muhammadrizuki8@gmail.com>
2021-01-13 13:04:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
     <body>
         <h1>hello world</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempora, animi veritatis modi molestiae dicta magni ab ex totam ea, deserunt et voluptates incidunt consectetur labore consequatur minus non explicabo.</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+        </ul>
     </body>
 </html>
